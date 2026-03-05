da3e1b9c233426e2f66eb84b9f46836abbc252a2
dhirajdhar <dhirajrohit.exe@gmail.com>
2026-03-05 20:21:07 +0000

    Update ata.h

--- DharOS/include/ata.h
+++ DharOS/include/ata.h
@@ -2,11 +2,10 @@
 #define ATA_H
 #include <stdint.h>
 
-// Initialize primary ATA bus, select master drive
 void ata_init();
 
-// Read/write a single 512-byte sector using LBA28
 int ata_read_sector(uint32_t lba, uint8_t* buf);
 int ata_write_sector(uint32_t lba, const uint8_t* buf);
+
 
 #endif
